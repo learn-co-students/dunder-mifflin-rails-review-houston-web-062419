6b18b238a8a4ba331c3156c5858b4d735bbc1767
Varvara Babkova <varvara.munday@gmail.com>
2019-07-24 14:40:27 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,12 @@
 class DogsController < ApplicationController
 	def index
 		@dogs = Dog.all
+		@dogs = @dogs.sort_by{|c| c.employees.count}
 	end
 
 	def show
 		@dog = Dog.find(params[:id])
+
 		@employees = @dog.employees
 	end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,4 +6,42 @@ class EmployeesController < ApplicationController
 	def show
 		@employee = Employee.find(params[:id])
 	end
+
+	def new
+		@employee = Employee.new
+	end
+
+	def create 
+		@employee = Employee.new(e_params)
+		if @employee.valid?
+			@employee.save
+			redirect_to employee_path(@employee)
+		else
+			flash[:errors] = @employee.errors.full_messages
+			redirect_to new_employee_path
+		end
+	end
+
+	def edit
+		@employee = Employee.find(params[:id])
+	end
+
+	def update
+		# byebug
+		@employee = Employee.new(e_params)
+
+		if @employee.valid?
+			@employee = Employee.find(params[:id])
+			@employee.update(e_params)
+			redirect_to employee_path(@employee)
+		else
+			flash[:errors] = @employee.errors.full_messages
+			redirect_to edit_employee_path
+		end
+
+	end
+
+	def e_params
+		params.require(:employee).permit(:first_name, :last_name, :dog_id, :title, :alias, :office)
+	end
 end
